c24c37983c4ec4018b3622cf0fce604f703ad377
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-28 19:39:36 -0300

    fix: the other pages besides App now are children of App

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,23 +1,31 @@
 import HomePage from "../pages/Homepage";
 import Cart from "../pages/Cart";
 import Shop from "../pages/Shop";
-import ErrorPage from "../pages/ErrorPage"
+import ErrorPage from "../pages/ErrorPage";
+import App from "../App";
 
 const routes = [
     {
         path: "/",
-        element: <HomePage />,
+        element: <App />,
         errorElement: <ErrorPage />,
-    },
-    {
-        path: "/shop",
-        element: <Shop />,
-        errorElement: <ErrorPage />,
-    },
-    {
-        path: "/cart",
-        element: <Cart />,
-        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/home",
+                element: <HomePage />,
+                errorElement: <ErrorPage />,
+            },
+            {
+                path: "/shop",
+                element: <Shop />,
+                errorElement: <ErrorPage />,
+            },
+            {
+                path: "/cart",
+                element: <Cart />,
+                errorElement: <ErrorPage />,
+            },
+        ]
     },
 ];
 
