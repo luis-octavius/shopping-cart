145621970f274eb410128f70cb789fe1ec4b5c45
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-28 18:05:10 -0300

    feat: error page component

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import './ErrorPage.css'
+
+export default function ErrorPage() {
+    return (
+        <div className="error">
+            <h1>Oh, no!</h1>
+            <p>There was an error</p>
+        </div>
+    )
+}
