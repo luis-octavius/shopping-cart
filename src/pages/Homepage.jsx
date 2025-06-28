df5e96f8beded6ac3a6a9325737202305db787ad
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-28 19:49:41 -0300

    refactor: remove navbar

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,11 @@
 import './Homepage.css'
 import Carousel from '../components/ui/Carousel';
 import { imgs } from '../assets/imgs';
-import Navbar from '../components/ui/Navbar';
 
 export default function HomePage () {
     return (
-        <>
-            <Navbar />
+        <div>
             <Carousel imgs={imgs}/>        
-        </>
+        </div>
     )
 }
