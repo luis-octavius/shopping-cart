2dbb8eb620bbd2158f606d9d6f9c75f7486f8322
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-29 22:24:15 -0300

    feat: loading animation, add key to Cards

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import Card from '../components/ui/Card';
+import Card from '../components/Card';
+import './Shop.css'
+import Loading from '../components/ui/Loading';
 
 export default function Shop() {
     const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    
     const url = 'https://fakestoreapi.com/products';
+
 
     useEffect(() => {
         fetch(url)
@@ -12,19 +17,34 @@ export default function Shop() {
         .then((response) => {
             setData(response);
             console.log(response);
+            setIsLoading(false);
         })
         .catch((error) => console.error(error))
-    },[url]);
+    },[isLoading]);
+
+    if (isLoading) return (
+        <div className="container-load">
+            <Loading />
+        </div>
+    )
 
     return (
+        <>
+            <div className="catalog">
+                <h1>Catalog</h1>
+            </div>
         <div className="container">
-            {data && data.map((product) => (
-                <Card props={product}/>
+            {data && data.map((item, index) => (
+                <Card 
+                    product={item}
+                    key={index}    
+                />
             ))}
         </div>
+        </>
     )
 }
 
 Shop.propTypes = {
-    url: PropTypes.string,
+    url: PropTypes.string.isRequired,
 }
