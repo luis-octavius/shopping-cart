cbe83b63dc4f86ce80d6f51ce94f5afc1b867229
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-29 22:14:50 -0300

    style: add h1 to show a greeting

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,15 @@
 import './Homepage.css'
-import Carousel from '../components/ui/Carousel';
+import Carousel from '../components/Carousel';
 import { imgs } from '../assets/imgs';
 
 export default function HomePage () {
     return (
         <div>
+            <h1 className="home-name">Welcome to <span
+                className="fade shop-name"
+            >Shoppex</span>
+
+            </h1>
             <Carousel imgs={imgs}/>        
         </div>
     )
