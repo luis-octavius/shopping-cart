622bcb9cebf9840905f3435cbb42ef26538b93b3
Big-Plato <luisoctavius.sc@gmail.com>
2025-07-04 19:49:55 -0300

    feat: useStore made with zustand to share the data of the items added to the cart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
 import './Card.css';
-import shopAdd from '../assets/shopping-cart-add.svg'
+import shopAdd from '../assets/shopping-cart-add.svg';
+import useStore from '../utils/store';
 
 export default function Card({product}) {
+    const { increment, addProduct } = useStore();
+
     return (
         <div className="card">
             <h3>{product.title}</h3> 
@@ -11,7 +14,14 @@ export default function Card({product}) {
             <div className="price-value">
                 <h4><span className="highlight">Price</span>R$ {product.price}</h4>
                 <h4><span className="highlight">Rating</span>{product.rating.rate}</h4>
-                <img className="add-button" src={shopAdd} />
+                <img 
+                    className="add-button" 
+                    src={shopAdd} 
+                    onClick={() => {
+                        increment(); 
+                        addProduct(product.id);
+                    }}
+                    />
             </div>
             <div className="card-description">
                 <p>{product.description}</p>
