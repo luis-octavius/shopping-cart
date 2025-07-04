41bca4ac46867aedf30f1312be99c363884c207f
Big-Plato <luisoctavius.sc@gmail.com>
2025-07-03 23:46:29 -0300

    add zustand function to handle state

---
+++ src/utils/store.js
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+
+const useStore = create((set) => ({
+    count: 0,
+    products: [],
+
+    increment: () => set((state) => ({ count: state.count + 1})),
+    decrement: () => set((state) => ({ count: state.count - 1})),
+    reset: () => set({ count: 0 }),
+    addProduct: (productId) => 
+        set((state) => ({ productId: [...state.products, productId]})),
+}));
+
+export default useStore;
