8d7e3183f5501bb6e937838d2578345047e4fb8a
Big-Plato <luisoctavius.sc@gmail.com>
2025-07-04 19:48:24 -0300

    feat: add context to handle the fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import { Outlet } from 'react-router';
 import Navbar from './components/Navbar';
+import { UserContext } from './utils/UserContext';
+import { useLoaderData } from 'react-router';
 
 function App () {
+  const data = useLoaderData();
+
   return (
     <>
+      <UserContext.Provider value={data}>
       <Navbar />
       <div>
         <Outlet />
       </div>
+      </UserContext.Provider>
     </>
   )
 }
