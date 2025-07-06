4705a17b6f27debb5fb746f41df6cc066ce28d6e
Big-Plato <luisoctavius.sc@gmail.com>
2025-07-06 18:07:42 -0300

    fix: add a empty cart div and proper price handling

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,38 +1,52 @@
 import './Cart.css';
 import ShopCard from '../components/ShopCard';
 import useStore from '../utils/store';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../utils/UserContext';
 
 export default function Cart () {
     const data = useContext(UserContext);
+    const [value, setValue] = useState(0);
     const { products } = useStore();
 
     useEffect(() => {
-        console.log("Data in Cart: ", data);
+        const values = products
+        .map((index) => {
+            const singleValue = handleProductPrice(index);
+            return singleValue;
+        })
+        .reduce((acc, curr) => acc + curr, 0);
+        setValue(values);
     }, [products]);
 
     const handleProductId = (id) => {
-        console.log(data[id - 1]);
         return data[id - 1];
     };
+
+    const handleProductPrice = (id) => {
+        return data[id - 1].price;
+    }
 
     return (
         <div className="shop">
             <div className="shop-items">
-                {products.map((index) => (
+                {products.length < 1 ? (
+                    <div className="empty">Carrinho Vazio</div>
+                ) : (products.map((index) => (
                     <ShopCard 
                         props={handleProductId(index)} 
                         key={index}
                     />
-                ))}
+                )))
+            }
+                
             </div>
             <div className="bar"></div>
             <div className="shop-buy">
                 <div className="value">
-                    <h2>Value</h2>
-                    <p>Aqui deverá ter uma função que somará tudo</p>
+                    <h2>Total Value<span className="highlight">R$ {value}</span></h2>
                 </div>
+                <button className="buy-btn">Buy</button>
             </div>
         </div>
     )
